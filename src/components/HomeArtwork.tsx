1708ab3ef58d3c116b7ec72b7542709c69b42c63
Jamison85 <jamisonr85@gmail.com>
2026-09-13 21:05:32 -0500

    land Findtrail trail directly on keys

--- src/components/HomeArtwork.tsx
+++ src/components/HomeArtwork.tsx
@@ -1,6 +1,6 @@
 const artworkUrl = `${import.meta.env.BASE_URL}home-memory-trail.webp`
 
-const trailPath = 'M1008 1080C914 974 860 884 892 804c18-46 64-65 104-92 68-46 129-93 168-157 41-67 56-134 22-183C1158 327 1118 298 1060 300'
+const trailPath = 'M1008 1080C914 974 860 884 892 804c18-46 64-65 104-92 68-46 129-93 168-157 41-67 56-134 22-183C1158 327 1122 300 1074 294'
 
 export function HomeArtwork() {
   return (
@@ -32,8 +32,8 @@ export function HomeArtwork() {
         <path className="home-artwork__flow-body" pathLength="100" d={trailPath} />
         <path className="home-artwork__flow-core" pathLength="100" d={trailPath} />
         <path className="home-artwork__flow-shimmer" pathLength="100" d={trailPath} />
-        <circle className="home-artwork__arrival-haze" cx="1060" cy="300" r="22" />
-        <circle className="home-artwork__arrival-dot" cx="1060" cy="300" r="4.5" />
+        <circle className="home-artwork__arrival-haze" cx="1074" cy="294" r="24" />
+        <circle className="home-artwork__arrival-dot" cx="1074" cy="294" r="5" />
       </svg>
     </div>
   )
